f32d61ee5b176f3dfb77acff348ba20d59705d36
Matheus Pereira Gonçalves Coelho <mpg9159@gmail.com>
2026-09-21 14:42:27 -0300

    Uptade: adicionando comentários

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,13 +6,14 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
+        //Variaveis
         int A, B, PROD;
-        
+        //Entrada de dados
         A = leia.nextInt();
         B = leia.nextInt();
-        
+        //Processamento
         PROD = (A * B);
-        
+        //Saida de dados
         System.out.println("PROD = " + PROD);
         
     }
